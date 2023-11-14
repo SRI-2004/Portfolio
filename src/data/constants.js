73fafc3ab0dc892c57595335d11eb1b366c1e300
SRI-2004 <116736492+SRI-2004@users.noreply.github.com>
2023-11-14 21:15:30 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,230 @@
+export const Bio = {
+  name: "Dhruv Gupta",
+  roles: [
+    "Cyber Security Enthusiast",
+    "Frontend Developer",
+    "Technophile",
+  ],
+  description:
+    "I'm a proactive and adaptable individual, constantly seeking fresh opportunities to excel. My fervor for continuous learning drives me to consistently produce top-notch outcomes. Coupled with an optimistic outlook and an unwavering commitment to personal growth, I am poised to make a significant impact and reach remarkable heights.",
+  github: "https://github.com/DDugs",
+  resume:
+    "https://www.linkedin.com/in/dhruv-vishesh-gupta-a98a84271/",
+  linkedin: "https://www.linkedin.com/in/dhruv-vishesh-gupta-a98a84271/",
+  insta: "https://instagram.com/notdhruvmore?igshid=OGQ5ZDc2ODk2ZA==/",
+  facebook: "https://www.facebook.com/profile.php?id=100035724995456&mibextid=9R9pXO/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Networking",
+        image:
+          "https://icons-for-free.com/iconfiles/png/512/share+logo+sharethis+sharethis+logo+social+media+icon-1320196090534622168.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.paloaltonetworks.com/content/dam/pan/en_US/images/logos/brand/primary-company-logo-color-white/Parent-logo-knockout.png?imbypass=on&imwidth=1920",
+    role: "Certified Cybersecurity Technician Intern",
+    company: "Palo Alto Networks",
+    date: "Sept 2023 - Nov 2023",
+    desc: "Learning the Fundamentals of Networking, Cloud Security, and other Securtiy Operations.",
+    skills: [
+      "Netowrking",
+      "Cloud Security",
+      "Cyber Security",
+      "Network Security",
+      "Security Operations",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://archaeobyte.online/static/img/logo-3.png",
+    role: "Web Dev Intern",
+    company: "ArchaeoBytes",
+    date: "Oct 2023 - Nov 2023",
+    desc: "Working on both frontend and backend of the web application using ReactJS, HTML, and CSS.",
+    skills: [
+      "ReactJS",
+      "Python",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://drive.google.com/file/d/1Lk4Nbx1GyhzgHYgbXpw5MralHvXxtcrz/view?usp=drivesdk",
+  },
+  {
+    id: 1,
+    img: "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/311501117_480430807444294_2698204105232936687_n.png?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=loqS7WIJhFsAX-B2KA0&_nc_ht=scontent-tir3-2.xx&oh=00_AfAK7aEpQIbJaBXgn8wo1tEz0-HZp9dhap76pxljkxDkQw&oe=6546BF9F",
+    role: "CEH V12 Certified Ethical Hacking Course",
+    company: "EC-Council",
+    date: "Dec 2023 - Present",
+    desc: "Getting training in ethical hacking and cybersecurity techniques to identify vulnerabilities and fortify systems.",
+    skills: [
+      "Peneteration Testing",
+      "Network Security",
+      "Ethical Hacking",
+      "Secure Coding Practices",
+      "Cryptography",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/f/fe/Srmseal.png",
+    school: "SRM Institute of Science and Technology, Kattankulathur",
+    date: "Sept 2022 - Sep 2026",
+    grade: "8.05 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering Specialization in Cyber Security at SRM Institute of Science and Technology, Kattankulathur. I have completed 2 semesters and have a CGPA of 8.05. I am currently taking courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of Aaruush Club at SRM, where I am still learning and participating in different events and competitions",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4D0BAQG0JNylY2loVQ/company-logo_200_200/0/1646672485865?e=1706745600&v=beta&t=VgCSnboN5S3Hs1J65lwIQduNAQxqNoDGIir-3u8Ueb8",
+    school: "Bal Bharati Public School, Noida",
+    date: "Apr 2021 - Apr 2022",
+    grade: "81.6%",
+    desc: "I completed my class 12 high school education at Bal Bharati Public School, Noida, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C4D0BAQG0JNylY2loVQ/company-logo_200_200/0/1646672485865?e=1706745600&v=beta&t=VgCSnboN5S3Hs1J65lwIQduNAQxqNoDGIir-3u8Ueb8",
+    school: "Bal Bharati Public School, Noida",
+    date: "Apr 2019 - Apr 2020",
+    grade: "80.2%",
+    desc: "I completed my class 10 education at Bal Bharati Public School, Noida.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 3,
+    title: "Mausam",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "A Weather App Mausam utilizing the OpenWeather API provides users with up-to-date weather information. It offers real-time data on temperature, humidity, wind speed, and precipitation for any location worldwide. Users can search for specific cities or enable location services for local forecasts. The app often features a user-friendly interface with intuitive icons and a Realtime forecast. Additionally, it may include extras like weather maps, UV indices, and sunrise/sunset times.",
+    image:
+      "https://user-images.githubusercontent.com/120174191/277790368-519d1ba4-302d-41b6-9f42-af08582f8dee.png",
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "OpenWeather API",
+    ],
+    category: "web app",
+    github: "https://github.com/DDugs/Mausam",
+    webapp: "/",
+  },
+  {
+    id: 2,
+    title: "E-Voting System",
+    date: "Aug 2023 - Sept 2023",
+    description:
+      "E-Voting System is a Desktop application designed to enables eligible voters to cast their ballots electronically, typically over the internet or through specialized electronic voting machines. It offers convenience and efficiency by allowing voters to participate in elections from their own devices or designated polling stations. E-voting systems aim to enhance accessibility, reduce human error, and expedite result tabulation. However, they also raise concerns about security, privacy, and the potential for tampering.",
+    image:
+      "https://user-images.githubusercontent.com/120174191/279380039-698813ad-3e04-4545-b91b-4b12698ac5d1.png",
+    tags: [
+      "Java",
+      "MySQL",
+    ],
+    category: "Desktop app",
+    github: "https://github.com/DDugs/E-Voting_System",
+    webapp: "/",
+  },
+  {
+    id: 1,
+    title: "VoiceChoice",
+    date: "Sept 2023",
+    description:
+      "VoiceChoice is a digital platform enabling voters to cast their ballots electronically. It typically involves secure authentication, allowing voters to participate remotely using computers or mobile devices. Voters log in, select their choices, and submit their votes through encrypted channels. E-voting systems offer increased accessibility, convenience, and potentially quicker results. They utilize various technologies to ensure security, including encryption, blockchain, or cryptographic techniques, safeguarding the integrity of the voting process.",
+    image:
+      "https://user-images.githubusercontent.com/120174191/279380516-de2b0a8a-e0a5-4b12-8b7f-4655d0dc49b9.png",
+    tags: ["HTML", "CSS", "JavaScript", "Python",],
+    category: "web app",
+    github: "https://github.com/DDugs/VoiceChoice",
+    webapp: "/",
+  },
+  {
+    id: 0,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
